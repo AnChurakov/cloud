7a6779703783bfb440cfa976cfecea37db0c1a82
Шишкодуй СС <plstsale@gmail.com>
2018-11-12 20:27:16 +0400

    Изменил шаблон админки

--- cms/resources/views/layouts/admin.blade.php
+++ cms/resources/views/layouts/admin.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html>
     <head>
         <meta charset="utf-8">
@@ -147,57 +146,6 @@
 
                 </nav>
 
-=======
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="assets/img/basic/favicon.ico" type="image/x-icon">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Paper</title>
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{asset('adminpanel/assets/css/app.css') }}">
-    <style>
-        .loader {
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background-color: #F5F8FA;
-            z-index: 9998;
-            text-align: center;
-        }
-
-        .plane-container {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-        }
-    </style>
-    <!-- Js -->
-    <!--
-    --- Head Part - Use Jquery anywhere at page.
-    --- http://writing.colin-gourlay.com/safely-using-ready-before-including-jquery/
-    -->
-    <script>(function(w,d,u){w.readyQ=[];w.bindReadyQ=[];function p(x,y){if(x=="ready"){w.bindReadyQ.push(y);}else{w.readyQ.push(x);}};var a={ready:p,bind:p};w.$=w.jQuery=function(f){if(f===d||f===u){return a}else{p(f)}}})(window,document)</script>
-</head>
-<body class="light">
-<!-- Pre loader -->
-<div id="loader" class="loader">
-    <div class="plane-container">
-        <div class="preloader-wrapper small active">
-            <div class="spinner-layer spinner-blue">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div><div class="gap-patch">
-                <div class="circle"></div>
-            </div><div class="circle-clipper right">
-                <div class="circle"></div>
-            </div>
->>>>>>> anton
             </div>
 
             <div class="spinner-layer spinner-red">
@@ -210,7 +158,6 @@
             </div>
             </div>
 
-<<<<<<< HEAD
 							<li class="has_sub">
                                 <a href="javascript:void(0);" class="waves-effect">
 								<i class="zmdi zmdi-local-offer"></i> <span> Промокоды </span> <span class="menu-arrow"></span></a>
@@ -248,17 +195,6 @@
                                     
                                 </ul>
                             </li>
-=======
-            <div class="spinner-layer spinner-yellow">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div><div class="gap-patch">
-                <div class="circle"></div>
-            </div><div class="circle-clipper right">
-                <div class="circle"></div>
-            </div>
-            </div>
->>>>>>> anton
 
             <div class="spinner-layer spinner-green">
                 <div class="circle-clipper left">
